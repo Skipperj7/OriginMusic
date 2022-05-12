3248c1bf409510805c4aa5a08f357a0b004195e7
Jacob <jacob.joc@gmail.com>
2022-05-11 23:18:44 -0700

    Created Simple File Upload to MongoDB from NodeJS using gridfs
    Uses multer middleware to upload files to gridfs api on mongo database. Switched to using mongoose for connection. 
    Makes use of ejs for graphics (although this will be replaced by react once front end dev catches up).
    
    Modeled on: https://www.youtube.com/watch?v=3f5Q9wDePzY

--- server/src/index.js
+++ server/src/index.js
@@ -1,35 +1,71 @@
-/*
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const {GridFsStorage} = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
 
 const app = express();
-const port = 420;
+//using ejs to test (replaced by react front end)
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.set('view engine','ejs');
+app.set('views', './views');
 
-app.get('/', (req, res) => {
-  console.log('Received get request!');
-  res.send('Hello World!');
+
+//MongoURI
+const mongoURI = 'mongodb://mongo:27017/uploads';
+
+// Create mongo connection
+const conn = mongoose.createConnection(mongoURI);
+
+// Init gfs
+let gfs;
+
+conn.once('open', () => {
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+// Create storage engine
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
 });
- */
+const upload = multer({ storage });
 
-const { MongoClient } = require("mongodb");
-// Connection URI
-const uri =
-    "mongodb://mongo:27017/";
-// Create a new MongoClient
-const client = new MongoClient(uri);
-async function run() {
-    try {
-        // Connect the client to the server
-        await client.connect();
-        // Establish and verify connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Connected successfully to server");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
-run().catch(console.dir);
+
+// @route GET /
+// @desc Loads form
+app.get('/', (req,res)=>{
+  res.render('index');
+});
+
+// @route POST /
+// @desc Uploads file to db
+app.post('/upload', upload.single('file'), (req,res)=>{
+  res.json({file: req.file});
+});
+
+const port =5000;
+
+app.listen(port, ()=> console.log('Server started on port: '+port.toString()));
+
