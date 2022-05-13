a0b014f5707f8ed9a90944ab8b5749aec937369e
Jacob <jacob.joc@gmail.com>
2022-05-13 11:07:19 -0700

    auth: Added base APIs for user auth
    Confirmed working signup using postman (note set Content-Type to application/json when posting since postman auto sets it to plaintext which fails).
    
    For user/me, put token into header with empty body.

--- server/src/index.js
+++ server/src/index.js
@@ -1,120 +1,30 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const crypto = require('crypto');
-const mongoose = require('mongoose');
-const multer = require('multer');
-const {GridFsStorage} = require('multer-gridfs-storage');
-const Grid = require('gridfs-stream');
-const methodOverride = require('method-override');
+const express = require("express");
+const bodyParser = require("body-parser");
+const user = require("./routes/user");
+const InitiateMongoServer = require("./config/db");
+
+// Initiate Mongo Server
+InitiateMongoServer();
 
 const app = express();
-//using ejs to test (replaced by react front end)
+
+// PORT
+const PORT = process.env.PORT || 4000;
+
+// Middleware
 app.use(bodyParser.json());
-app.use(methodOverride('_method'));
-app.set('view engine','ejs');
-app.set('views', './src/views'); //set to ./views for docker, ./src/views for dev
 
-
-//MongoURI
-//set to mongo for docker use, localhost for dev
-const mongoURI = 'mongodb://localhost:27017/uploads';
-
-// Create mongo connection
-const conn = mongoose.createConnection(mongoURI);
-
-// Init gfs
-let gfs;
-
-conn.once('open', () => {
-  // Init stream
-  gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection('uploads');
+app.get("/", (req, res) => {
+  res.json({ message: "API Working" });
 });
 
-// Create storage engine
-const storage = new GridFsStorage({
-  url: mongoURI,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: 'uploads'
-        };
-        resolve(fileInfo);
-      });
-    });
-  }
+/**
+ * Router Middleware
+ * Router - /user/*
+ * Method - *
+ */
+app.use("/user", user);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server Started at PORT ${PORT}`);
 });
-const upload = multer({ storage });
-
-
-// @route GET /
-// @desc Loads form
-app.get('/', (req,res)=>{
-  res.render('index');
-});
-
-// @route POST /
-// @desc Uploads file to db
-app.post('/upload', upload.single('file'), (req,res)=>{
-  res.json({file: req.file});
-});
-
-// @route GET /image/:filename
-// @desc Display Image
-app.get('/image/:filename', (req, res) => {
-  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-    // Check if file
-    if (!file || file.length === 0) {
-      return res.status(404).json({
-        err: 'No file exists'
-      });
-    }
-
-    // Check if image
-    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
-      // Read output to browser
-      const readstream = gfs.createReadStream(file.filename);
-      readstream.pipe(res);
-    } else {
-      res.status(404).json({
-        err: 'Not an image'
-      });
-    }
-  });
-});
-
-// @route GET /audio/:filename
-// @desc Display Audio File
-app.get('/audio/:filename', (req, res) => {
-  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-    // Check if file
-    if (!file || file.length === 0) {
-      return res.status(404).json({
-        err: 'No file exists'
-      });
-    }
-
-    // Check if image
-    if (file.contentType === 'audio/mpeg') {
-      // Read output to browser
-      const readstream = gfs.createReadStream(file.filename);
-      readstream.pipe(res);
-    } else {
-      res.status(404).json({
-        err: 'Not an mp3'
-      });
-    }
-  });
-});
-
-const port =5000;
-
-app.listen(port, ()=> console.log('Server started on port: '+port.toString()));
-
