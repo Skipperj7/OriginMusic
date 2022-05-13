e1073a6b3dd6b1b2c37dc2473fed148bb9265e64
Jacob <jacob.joc@gmail.com>
2022-05-12 21:19:34 -0700

    API: Added ability to grab images and audio files
    Uses /images and /audio to expose database requests for uploaded images and audio files.
    Audio files must be mpeg. Images must be jpg or png. 
    Uses /images/filename for query.

--- server/src/index.js
+++ server/src/index.js
@@ -13,11 +13,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(methodOverride('_method'));
 app.set('view engine','ejs');
-app.set('views', './views');
+app.set('views', './src/views'); //set to ./views for docker, ./src/views for dev
 
 
 //MongoURI
-const mongoURI = 'mongodb://mongo:27017/uploads';
+//set to mongo for docker use, localhost for dev
+const mongoURI = 'mongodb://localhost:27017/uploads';
 
 // Create mongo connection
 const conn = mongoose.createConnection(mongoURI);
@@ -63,6 +64,54 @@ app.get('/', (req,res)=>{
 // @desc Uploads file to db
 app.post('/upload', upload.single('file'), (req,res)=>{
   res.json({file: req.file});
+});
+
+// @route GET /image/:filename
+// @desc Display Image
+app.get('/image/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+
+    // Check if image
+    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an image'
+      });
+    }
+  });
+});
+
+// @route GET /audio/:filename
+// @desc Display Audio File
+app.get('/audio/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+
+    // Check if image
+    if (file.contentType === 'audio/mpeg') {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an mp3'
+      });
+    }
+  });
 });
 
 const port =5000;
