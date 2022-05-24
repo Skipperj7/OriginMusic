d514f05038a7606211ff6e93dbde64af9650664b
Jacob <jacob.joc@gmail.com>
2022-05-23 23:12:17 -0700

    Merged Database Dev with User Auth

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const user = require("./routes/user");
 const InitiateMongoServer = require("./config/db");
+const methodOverride = require('method-override');
+const upload = require("./routes/upload");
 
 // Initiate Mongo Server
 InitiateMongoServer();
@@ -13,9 +15,14 @@ const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.set('view engine','ejs');//using ejs to test (replaced by react front end)
+app.set('views', './src/views'); //set to ./views for docker, ./src/views for dev
 
-app.get("/", (req, res) => {
-  res.json({ message: "API Working" });
+// @route GET /
+// @desc Loads form
+app.get('/', (req,res)=>{
+  res.render('index');
 });
 
 /**
@@ -24,6 +31,12 @@ app.get("/", (req, res) => {
  * Method - *
  */
 app.use("/user", user);
+/**
+ * Router Middleware
+ * Router - /upload/*
+ * Method - *
+ */
+app.use("/upload", upload);
 
 app.listen(PORT, (req, res) => {
   console.log(`Server Started at PORT ${PORT}`);
---
+++ server/src/routes/upload.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const router = express.Router();
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const {GridFsStorage} = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const multer = require('multer');
+
+const mongoURI = "mongodb://localhost:27017/uploads";
+
+// Create mongo connection
+const conn = mongoose.createConnection(mongoURI);
+
+// Init gfs
+let gfs;
+
+conn.once('open', () => {
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
+// Create storage engine
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({ storage });
+
+
+// @route POST /
+// @desc Uploads file to db
+router.post('/', upload.single('file'), (req,res)=>{
+  res.json({file: req.file});
+});
+
+// @route GET /image/:filename
+// @desc Display Image
+router.get('/image/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+
+    // Check if image
+    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an image'
+      });
+    }
+  });
+});
+
+// @route GET /audio/:filename
+// @desc Display Audio File
+router.get('/audio/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+
+    // Check if image
+    if (file.contentType === 'audio/mpeg') {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an mp3'
+      });
+    }
+  });
+});
+module.exports = router;
